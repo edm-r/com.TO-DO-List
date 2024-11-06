08937e3b822ece6bb05c348ce8e97e04923ebeae
NFNB1969 <fredborelnguele19@gmail.com>
2024-11-06 02:16:49 +0100

    fonction pour verifier les taches en retard

--- init.js
+++ init.js
@@ -1,2 +1,5 @@
 // Charger les tâches au démarrage
 document.addEventListener('DOMContentLoaded', displayTasks);
+
+// Appel de la fonction toutes les 60 secondes pour vérifier les tâches en retard
+setInterval(checkOverdueTasks, 5000); // Vérifie toutes les 5 secondes
